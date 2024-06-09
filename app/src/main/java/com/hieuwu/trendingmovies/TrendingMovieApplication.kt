47fa93c2b0216986fe3f330ca1d011a9aefd15b2
Hieu Vu <star.hieu.99@gmail.com>
2024-06-09 10:29:19 +0700

    Set up di

---
+++ app/src/main/java/com/hieuwu/trendingmovies/TrendingMovieApplication.kt
@@ -0,0 +1,9 @@
+package com.hieuwu.trendingmovies
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TrendingMovieApplication : Application() {
+
+}
